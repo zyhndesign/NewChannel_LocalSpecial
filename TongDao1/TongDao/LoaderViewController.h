dbcfea84e5805562d702bc06e3951c2a5c8134c0
sunyong445 <sunyong@sunyongtekiMacBook-Pro.local>
2013-12-09 10:35:40 +0800

    update

---
+++ TongDao1/TongDao/LoaderViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoaderViewController.h
+//  TongDao
+//
+//  Created by sunyong on 13-12-9.
+//  Copyright (c) 2013年 sunyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoaderViewController : UIViewController
+
+@end
---
+++ TongDao1/TongDao/LoaderViewController.m
@@ -0,0 +1,38 @@
+//
+//  LoaderViewController.m
+//  TongDao
+//
+//  Created by sunyong on 13-12-9.
+//  Copyright (c) 2013年 sunyong. All rights reserved.
+//
+
+#import "LoaderViewController.h"
+
+@interface LoaderViewController ()
+
+@end
+
+@implementation LoaderViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
